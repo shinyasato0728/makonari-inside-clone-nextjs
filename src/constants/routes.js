401d454a2e1c8f41b57c44239ba112878a72be8a
Shinya Sato <shinyasato@Shinya-SatonoMacBook-Pro.local>
2020-11-08 22:05:57 +0900

    update

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 export const HOME = '/';
-export const GITHUB = 'https://github.com/shinyasato0728';
+export const GITHUB = 'https://github.com/shinyasato0728/makonari-inside-clone-nextjs';
 export const SIGN_IN = '/signin';
 export const SIGN_UP = '/signup';
 export const PW_FORGET = '/pw-forget';
-export const SITE_URL = 'https://shinya-sato.com';
+export const SITE_URL = 'https://makonari-inside-clone.netlify.app';
